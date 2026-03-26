2275761d61604d23e49d93c85178a8ca57a3688c
Jacob Huynh <jacobshuynh2@gmail.com>
2026-03-26 02:18:42 -0400

    refactored submitjob and added submitbatch

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"io"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -25,21 +26,74 @@ func New(d *db.DB) *ETLServer {
 }
 
 func (s *ETLServer) SubmitJob(ctx context.Context, req *pb.JobRequest) (*pb.JobResponse, error) {
-	status, err := s.db.GetJobByIdempotencyKey(ctx, req.IdempotencyKey)
-	if err != nil && err != pgx.ErrNoRows {
-		return nil, err
-	}
-
-	if err == nil {
-		return &pb.JobResponse{JobId: status.JobId, State: pb.JobState_JOB_STATE_QUEUED, Message: "Job already exists with the same idempotency key.", CreatedAt: status.CreatedAt}, nil
-	}
-
-	jobId, err := s.db.InsertJob(ctx, req)
+	jobId, jobCreated, err := s.processJob(ctx, req)
 	if err != nil {
 		return nil, err
+	}
+
+	if !jobCreated {
+		return &pb.JobResponse{JobId: jobId, State: pb.JobState_JOB_STATE_QUEUED, Message: "Job already exists with the same idempotency key.", CreatedAt: timestamppb.New(time.Now())}, nil
 	}
 
 	s.jobQueue <- req
 
 	return &pb.JobResponse{JobId: jobId, State: pb.JobState_JOB_STATE_QUEUED, Message: "Job queued successfully.", CreatedAt: timestamppb.New(time.Now())}, nil
+}
+
+func (s *ETLServer) SubmitBatch(stream pb.ETLService_SubmitBatchServer) error {
+	var totalReceived, totalAccepted, totalRejected int32
+	var results []*pb.BatchJobResult
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.BatchResponse{TotalReceived: totalReceived, TotalAccepted: totalAccepted, TotalRejected: totalRejected, Results: results})
+		}
+		if err != nil {
+			return err
+		}
+
+		totalReceived++
+
+		jobId, jobCreated, err := s.processJob(stream.Context(), req)
+		if err != nil || !jobCreated {
+			totalRejected++
+		} else {
+			totalAccepted++
+			s.jobQueue <- req
+			results = append(results, &pb.BatchJobResult{
+				JobId:          jobId,
+				State:          pb.JobState_JOB_STATE_QUEUED,
+				IdempotencyKey: req.IdempotencyKey,
+				Message:        "Job processed successfully.",
+			})
+		}
+	}
+}
+
+func (s *ETLServer) GetJobStatus(ctx context.Context, req *pb.GetJobStatusRequest) (*pb.JobStatus, error) {
+	status, err := s.db.GetJobByID(ctx, req.JobId)
+	if err != nil {
+		return nil, err
+	}
+
+	return status, nil
+}
+
+// Helpers
+func (s *ETLServer) processJob(ctx context.Context, req *pb.JobRequest) (string, bool, error) {
+	status, err := s.db.GetJobByIdempotencyKey(ctx, req.IdempotencyKey)
+	if err != nil && err != pgx.ErrNoRows {
+		return "", false, err
+	}
+
+	if err == nil {
+		return status.JobId, false, nil
+	}
+
+	JobId, err := s.db.InsertJob(ctx, req)
+	if err != nil {
+		return "", false, err
+	}
+	return JobId, true, nil
 }
